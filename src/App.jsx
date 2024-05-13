53f774c144f133c51d1ffca5f1f992a5cef03b01
Tejasmagdum2309 <magdumtej1008@gmail.com>
2024-05-13 16:24:28 +0530

    Basic things setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Router,Routes } from 'react-router-dom';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+       </h1> */}
+    </Routes>
+  )
+}
+
+export default App
